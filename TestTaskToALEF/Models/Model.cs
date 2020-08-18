6f90956b56ffecfe1dee79d4941fdc2749d4414e
sviuz <kuibidanill@gmail.com>
2020-08-18 11:39:17 +0300

    Add project files.

---
+++ TestTaskToALEF/Models/Model.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestTaskToALEF.Models
+{
+    public class Model
+    {
+        public int Id { get; set;}
+        [Required]
+        [MinLength(3), MaxLength(3)]
+        public int Value { get; set; }
+        [Required]
+        [MaxLength(20)]
+        public string Name { get; set; }
+    }
+}
