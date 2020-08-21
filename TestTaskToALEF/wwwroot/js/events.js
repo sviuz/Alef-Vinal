526356587535ac353cd177be4ce0e96c8c79535c
sviuz <kuibidanill@gmail.com>
2020-08-21 14:41:35 +0300

    css + js + html

--- TestTaskToALEF/wwwroot/js/events.js
+++ TestTaskToALEF/wwwroot/js/events.js
@@ -1,1 +1,9 @@
-﻿
+﻿var trArray = document.getElementsByClassName('list-table-tr');
+
+for (var i = 0; i < trArray.length; i++) {
+    var tr = trArray[i];
+    tr.onclick = function () {
+        let trAttr = this.attributes['data-id'].textContent;
+        alert(`ID: ${trAttr}`);
+    }
+}
